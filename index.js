9a719dd63fd3c6ff368ed6c50fb7b22d96771834
Peter Njiru <peternjirum@gmail.com>
2023-03-11 14:29:53 -0500

    completed Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "12th Avenue",
+};
+
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employeeClone, key, value) {
+    employeeClone = { ...employee };
+    employeeClone[key] = value;
+    return employeeClone;
+};
+
+const employeeCopy = updateEmployeeWithKeyAndValue(employee, "streetAddress", "11 Broadway");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+};
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "Tom")
+
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeCopy2 = { ...employee };
+    delete employeeCopy2[key];
+    return employeeCopy2;
+}
+
+const newEmployee = deleteFromEmployeeByKey(employee, "name");
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+destructivelyDeleteFromEmployeeByKey(employee, "name");
+
